8cbb8fd50ba08ef2dde4a8b5cb9b7cc82fff280c
karuna ks <myemail@example.com>
2023-10-03 21:41:42 -0400

    committed

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function toggleMode() {
   modeStatus.innerText = "Currently in " + modeMessage;
 }
 modeToggle.addEventListener('click', toggleMode);
+
+// for fade and slide on scroll
+const items = document.querySelectorAll('.item:not(:first-child)');
+const options = {
+  threshold: 0.5
+}
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options)
+items.forEach(item => {
+  observer.observe(item);
+})
